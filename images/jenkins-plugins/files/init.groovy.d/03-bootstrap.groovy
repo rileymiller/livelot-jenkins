24f141fc6a15c81403ae2457030541231f3e2ea5
Riley Miller <rmllr000@gmail.com>
2021-02-28 22:36:52 -0700

    fix: the pipeline job path done got borked up

--- images/jenkins-plugins/files/init.groovy.d/03-bootstrap.groovy
+++ images/jenkins-plugins/files/init.groovy.d/03-bootstrap.groovy
@@ -5,4 +5,9 @@ def jobDslScript = new File('/usr/share/jenkins/ref/bootstrap/bootstrap.groovy')
 def workspace = new File('.')
 def jobManagement = new JenkinsJobManagement(System.out, [:], workspace)
 
+println("INFO: Job Management" + jobManagement)
+
 new DslScriptLoader(jobManagement).runScript(jobDslScript.text)
+
+println("INFO: We've read in the bootstrap script" + jobManagement)
+
