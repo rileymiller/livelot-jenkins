6bf89c73dc1f96ed4d20b8ccb5b615a767c3a11b
Riley Miller <rmllr000@gmail.com>
2021-02-28 22:51:28 -0700

    fix: ammend the folder path

--- dsl/pipeline_test/pipeline_test.groovy
+++ dsl/pipeline_test/pipeline_test.groovy
@@ -2,7 +2,7 @@ folder('pipeline_test') {
   displayName('Pipeline Test')
   description('Folder for the pipeline test example')
 }
-pipelineJob('pipeline job test') {
+pipelineJob('pipeline_test/example') {
   definition {
     cps {
       script(readFileFromWorkspace("dsl/pipeline_test/Jenkinsfile"))
